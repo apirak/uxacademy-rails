1d2d2fb4ee7c64abca46c482ef049f234c4eacb0
Apirak Panatkool <apirakb@gmail.com>
2017-04-24 23:53:22 +0700

    new model and active admin

---
+++ db/migrate/20170424163005_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.date :start_date
+      t.decimal :period
+      t.decimal :max_price
+      t.decimal :low_price
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
