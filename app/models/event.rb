ea789c48c9e4fae6515e5fa509feaa672214e5e9
Apirak Panatkool <apirakb@gmail.com>
2017-06-26 22:53:11 +0700

    edit event order in home

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ApplicationRecord
 
   delegate :url_helpers, to: 'Rails.application.routes'
 
+  default_scope { order(start_date: :asc) }
+
   def detail_url
     if start_date > Date.today
       if url.blank?
